236256e8cad9e8af6b73a91a7d33aa1cba1cc758
Kaito <kaitomuzicvn@gmail.com>
2026-08-26 11:45:27 +0700

    ready3

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,28 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
-import path from 'path';
 
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
-
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth", authRoutes);
+// Middleware parse JSON
+app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-  })
-}
+// Health check endpoint cho Render
+app.get("/", (req, res) => {
+  res.send("Chatify Backend API is running...");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
