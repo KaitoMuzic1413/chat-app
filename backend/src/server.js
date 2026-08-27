9b6725aa3a0062b6d4defa441d558849c2f268e7
Kaito <kaitomuzicvn@gmail.com>
2026-08-27 16:12:44 +0700

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from "path";
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { connectDB } from './libs/db.js';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,4 +24,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
